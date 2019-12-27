b77ce39010989f2fdb043a400c314fc771981c0c
san4o101 <snikersmc123@gmail.com>
2019-12-27 21:23:51 +0200

    add constants to bil requests model. add validation job data before insert to table.

--- app/jobs/bills_create_job.rb
+++ app/jobs/bills_create_job.rb
@@ -1,10 +1,21 @@
 class BillsCreateJob < ApplicationJob
   queue_as :default
 
-  def perform(user, type_bill)
+  before_enqueue do |job|
+    user = job.arguments[0]
+    type_bill = job.arguments[1]
+
     card_number = generate_number
     card_number = generate_number while Bill.find_by_card_number(card_number)
 
+    bill = Bill.new(users_id: user.id, bill_type: type_bill,
+                    card_number: card_number)
+    throw(:abort) unless bill.valid?
+
+    job.arguments[2] = card_number
+  end
+
+  def perform(user, type_bill, card_number = nil)
     Bill.create(users_id: user.id, bill_type: type_bill, amount: 0,
                 percent: 0, card_number: card_number,
                 expired_bill_at: 5.year.from_now)
--- app/models/bill_request.rb
+++ app/models/bill_request.rb
@@ -1,4 +1,8 @@
 class BillRequest < ApplicationRecord
   belongs_to :user, foreign_key: 'admin_id'
   belongs_to :bill, foreign_key: 'bill_id'
+
+  APPROVED_YES = 1
+  APPROVED_NO = 2
+
 end
